ed18fbd79b777cf9d29355fb6644e71251a955a2
xuxingxing <973553289@qq.com>
2020-06-18 23:18:59 +0800

    vue methods

--- vue_data and vue_el/app.js
+++ vue_data and vue_el/app.js
@@ -2,11 +2,29 @@
 // 传递一个对象
 new Vue({
   el: '#app',// el表示vue这个实例要操作哪个容器,只能在app这个div中使用
+  //data本身是一个函数
   data() {
     return {
       name: 'mrokxu',
       age: 18
     }
+  },
+  methods: {
+    // 写法一
+    // greet: function () {
+    //   return "Good night" +  this.name
+    // },
+    // 写法二 es6
+    // greet() {
+    //   return `Good night${this.name}`
+    // },
+    // 传参
+    greet(time) {
+      return `Good ${time} ${this.name}`
+    },
+    // 多个方法
+    haveLunch() {
+      return `吃过午饭了`
+    }
   }
-  //data本身是一个函数
 })
