198102c99e826337db267d30241b150bdad6ace6
xuxingxing <973553289@qq.com>
2020-06-19 23:30:44 +0800

    vue_computed

---
+++ vue_computed/app.js
@@ -0,0 +1,30 @@
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      a: 0,
+      b: 0,
+      age: 10
+    }
+  },
+  methods: {
+    // addToA() {
+    //   console.log('addToA')
+    //   return this.a + this.age
+    // },
+    // addToB() {
+    //   console.log('addToB')
+    //   return this.b + this.age
+    // }
+  },
+  computed: {
+    addToA() {
+      console.log('addToA')
+      return this.a + this.age
+    },
+    addToB() {
+      console.log('addToB')
+      return this.b + this.age
+    }
+  }
+})
