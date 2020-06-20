b4a362f74702bbfe498228d8041b17525b0f3daa
xuxingxing <973553289@qq.com>
2020-06-20 09:00:49 +0800

    vue_v-for

---
+++ vue_v-for/app.js
@@ -0,0 +1,14 @@
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      characters: ["小猪佩奇","小羊苏西", "小兔黎贝卡", '大象艾米丽'],
+      users: [
+        {name: '小猪佩', age: 3},
+        {name: '小羊苏西', age: 3},
+        {name: '小兔黎贝卡', age: 5},
+        {name: '大象艾米丽', age: 6}
+      ]
+    }
+  }
+})
